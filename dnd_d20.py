1c812976403ec117800cd1f974065048173cb16c
Joe Phelps <joephelps@gmail.com>
2018-03-04 19:23:16 -0500

    Additional features
    Added a pixel art skull to critical hit roll.

--- dnd_d20.py
+++ dnd_d20.py
@@ -1,19 +1,36 @@
 from sense_hat import SenseHat
 import random
+import time
 
-speed = 0.05
 
 sense = SenseHat()
 sense.clear()
+
+speed = 0.05
+x = [255,255,0]
+o = [0,0,0]
+
+skull = [
+o,o,x,x,x,x,o,o,
+o,x,x,x,x,x,x,o,
+o,x,o,x,x,o,x,o,
+o,x,x,x,x,x,x,o,
+o,x,x,o,o,x,x,o,
+o,o,x,x,x,x,o,o,
+o,o,x,o,o,x,o,o,
+o,o,o,o,o,o,o,o
+]
 
 sense.show_message("Let's Roll!", speed)
 def roll_dice():
     r = random.randint(1, 20)
     if r < 20:
-    for r in roll_dice     
         sense.show_message(str(r), speed)
     elif r == 20:
-        sense.show_message("Critical Hit!")
+        sense.show_message("Critical Hit!", speed,
+                text_colour=[255,255,0],
+                back_colour=[0,0,0])
+        sense.set_pixels(skull)
 
 while True:
     x, y, z = sense.get_accelerometer_raw().values()
